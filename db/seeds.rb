0b58955e2bf81bcd2dcc945c09d24f320a643d89
bubsworth <bah1996@hotmail.co.uk>
2021-11-04 16:40:44 +0000

    Update schema/update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Post.delete_all
+
+puts "\n== Creating users =="
+
+user_1 = User.create!(
+  username: "steve_man",
+  email: "steve@example.com",
+  password: "secret123",
+)
+
+puts "\n== Creating posts =="
+
+post_1 = Post.create!(
+  caption: "Missing the beach!",
+  user_id: user_1
+)
+
+post_2 = Post.create!(
+  caption: "Grabbing a coffee with the gang",
+  user_id: user_1
+)
+
+puts "\n== Handling active images =="
+
+def path_for_image(filename)
+  "app/assets/images/#{filename}"
+end
+
+# ActiveStorage attachments
+post_1.image.attach(io: File.open(path_for_image("beach.png")), filename: "beach.png")
+post_1.save!
+post_2.image.attach(io: File.open(path_for_image("coffee.png")), filename: "coffee.png")
+post_2.save!
+
