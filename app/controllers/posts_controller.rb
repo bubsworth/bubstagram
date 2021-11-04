c21b70816136ad3531c82b5f0c5ea1ccafc33a42
bubsworth <bah1996@hotmail.co.uk>
2021-11-04 16:48:23 +0000

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,50 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :destroy, :show, :update]
+
 	def index
+    @posts = Post.all
 	end
 
 	def show
 	end
 
 	def new
+    @post = Post.new
 	end
 
 	def create
+    @post = Post.create(post_params)
+
+		if @post.save
+		  redirect_to posts_path, notice: "Posted!"
+		else
+		  render :new
+		end
 	end
 
 	def edit
 	end
 
 	def update
+		if @post.update(post_params)
+		  redirect_to posts_path, notice: "Posted!"
+		else
+		  render :edit
+		end
 	end
-
-	def destroy
-	end
+	
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "Post was deleted" 
+  end
 
 	private
 
-		def post_params
-			require.params(:post).()
-		end
+    def post_params
+      require.params(:post).permit(:image, :caption)
+    end
 
-		def set_post
-
-		end
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
